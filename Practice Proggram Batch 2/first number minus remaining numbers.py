8f94e81940ab8f26d1afd395fef49d40c9836d66
biancaxby <paulabiancapiquero@gmail.com>
2025-03-05 21:59:56 +0800

    yippe

--- Practice Proggram Batch 2/first number minus remaining numbers.py
+++ Practice Proggram Batch 2/first number minus remaining numbers.py
@@ -1,7 +1,7 @@
 #Print the result of the first number minus all of the remaining numbers.
 total = 0
 num = int(input('type in number: '))
-for i in range(1,4):
+for i in range(1,10):
     total += int(input(f'enter a number: '))
     x = num - total
 
