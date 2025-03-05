1aad27d339cad40fd88f9eaceb17a9ac40695f0a
biancaxby <paulabiancapiquero@gmail.com>
2025-03-05 09:50:44 +0800

    yuh

--- Practice Proggram Batch 2/odd numbers 0-100.py
+++ Practice Proggram Batch 2/odd numbers 0-100.py
@@ -1,9 +1,11 @@
 #Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
-oms = 0
-while oms <= 100:
-        oms += 1
-        f = oms /2 != 0
+oms = 1
 
-print (f)
+while oms <100:
+        print(oms)
+        oms += 2
+        continue
+
+print (oms)
             
     
