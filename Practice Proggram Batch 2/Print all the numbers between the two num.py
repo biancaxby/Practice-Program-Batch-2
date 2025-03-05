13b478dbffe29341343d5e151340a6e9b6069e6c
biancaxby <paulabiancapiquero@gmail.com>
2025-03-05 16:14:20 +0800

    yippe

--- Practice Proggram Batch 2/Print all the numbers between the two num.py
+++ Practice Proggram Batch 2/Print all the numbers between the two num.py
@@ -1,3 +1,19 @@
 #Print all the numbers between the two numbers.
-x = int(input("Enter a number: "))
-y = int(input('Enter a number: '))
+x = int(input("input number: "))
+y = int(input("input another: "))
+  
+if x>y:
+    x = y
+    y = x
+    
+print(f'{x}')
+for i in range(x + 1, y):
+    print(i)
+print(y)
+           
+        
+        
+        
+            
+        
+
--- Practice Proggram Batch 2/quotient.py
+++ Practice Proggram Batch 2/quotient.py
@@ -1,5 +1,5 @@
 # Print the quotient of the two numbers without the decimal point
 x = float(input('Enter a number: '))
 y = float(input('Divide with?: '))
-h = x / y
+h = int(x / y)
 print(h)
